9f56666bf72b8e3bbf5c2b6d060f1275375f0848
Alexey Beall <alexeybeall@D-173-250-188-165.dhcp4.washington.edu>
2017-10-21 22:11:13 -0700

    Fixed error methods

--- index.js
+++ index.js
@@ -7,31 +7,34 @@
     function startRecord() {
 	var step1 = new XMLHttpRequest();
 	step1.open("GET", "src/speechToText.py");
-	step1.onload = new function() {
+	var step1Error = new function() {
+	    console.log("Could not record from mic");
+	};
+	step1.onload = function() {
 	    if(this.status >= 200 && this.status < 300) {
 		var step2 = new XMLHttpRequest();
 		var input = this.responseText;
 		step2.open("POST", "src/calculator.py");
+		var step2Error = new function() {
+		    console.log("Could not convert to solution");
+		};
 		step2.onload = new function() {
 		    if(this.status >= 200 && this.status < 300) {
 			console.log(this.responseText);
 		    } else {
-			step2.onerror();
+			step2Error();
 		    }
 		};
-		step2.onerror = new function() {
-		    console.log("Could not convert to solution");
-		};
+		step2.onerror = step2Error;
 		var inputData = new FormData();
 		inputData.append("text", this.responseText);
 		step2.send(inputData);
 	    } else {
-		step1.onerror();
+		step1Error();
 	    }
 	};
-	step1.onerror = function() {
-	    console.log("Could not record from mic");
-	};
+	step1.onerror = step1Error;
+	step1.send();
 	//rec.record();
 	//document.getElementById("button").onclick = stopRecord;
     }
