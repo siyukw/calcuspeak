af07703481935a54eefc4b14d69981a7f5470596
“Siyu <siyukw@cs.washington.edu>
2017-10-22 04:12:34 -0700

    used flask, pulled upstream

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import calculator, textToAudio, audioToText
 
-data = audioToText.audioToText()
-print(data)
-sol = calculator.calculate(data)
-print(sol)
-textToAudio.text_to_speech(sol)
+def q():
+    data = audioToText.audioToText()
+    print(data)
+    sol = calculator.calculate(data)
+    print(sol)
+    textToAudio.text_to_speech(sol)
